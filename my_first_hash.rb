77aea0e48df2162feb37378340c5ca06f21c7fd7
Camiyah Goins <tokyonote9@gmail.com>
2020-02-22 17:15:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,16 +10,16 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
-shipping_manifest = {
+
+end
+
+def retrieval
+  shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
 
-end
-
-def retrieval
-  
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
